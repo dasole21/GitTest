14375ce48d107cc0d365d2b9a4c195703e2c6f31
ds.kang <dasole21@naver.com>
2022-01-08 21:10:44 +0900

    add view2

---
+++ GitTest/view/View2.swift
@@ -0,0 +1,20 @@
+//
+//  View2.swift
+//  GitTest
+//
+//  Created by kangdasol on 2022/01/08.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View2_Previews: PreviewProvider {
+    static var previews: some View {
+        View2()
+    }
+}
