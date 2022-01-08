2d24cde48c62bee5ec5b30b7b674379fd17bc67c
ds.kang <dasole21@naver.com>
2022-01-08 23:20:52 +0900

    fix StringUtil

--- GitTest/util/StringUtil.swift
+++ GitTest/util/StringUtil.swift
@@ -9,14 +9,17 @@ import Foundation
 
 struct StringUtil {
     static func getFirst(text: String) -> Character? {
+        print("getFirst, text : \(text)")
         return text.first
     }
     
     static func getLast(text: String) -> Character? {
+        print("getLast, text : \(text)")
         return text.last
     }
     
     static func getLast2(text: String) -> Character? {
+        print("getLast2, text : \(text)")
         return text.last
     }
 }
