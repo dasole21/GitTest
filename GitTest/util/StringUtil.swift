7d4a810eb2940cad14402bcc3a0b31e4d83b0ac6
ds.kang <dasole21@naver.com>
2022-01-08 22:46:45 +0900

    add getLast

--- GitTest/util/StringUtil.swift
+++ GitTest/util/StringUtil.swift
@@ -11,4 +11,9 @@ struct StringUtil {
     static func getFirst(text: String) -> Character? {
         return text.first
     }
+    
+    static func getLast(text: String) -> Character? {
+        return text.last
+    }
+
 }
